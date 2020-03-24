51b4cba0f3cd4ee4a78a46c20658ef3e9d497f75
Justin Price <mr.price@Justins-iMac.local>
2020-03-24 11:07:33 -0400

    commented out router, fixed message in indexjs

--- emails/emailRouter.js
+++ emails/emailRouter.js
@@ -1,37 +1,37 @@
-const express = require('express');
+// const express = require('express');
 
-const Emails = require('./emailModel');
+// const Emails = require('./emailModel');
 
-const router = express.Router();
+// const router = express.Router();
 
 
 
-router.post('/', validateEmail, (req, res) => {
-  const emailData = req.body;
-  Emails.add(emailData)
-  .then(email => {
-    res.status(201).json(email);
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to send email' });
-  });
-});
+// router.post('/', validateEmail, (req, res) => {
+//   const emailData = req.body;
+//   Emails.add(emailData)
+//   .then(email => {
+//     res.status(201).json(email);
+//   })
+//   .catch (err => {
+//     res.status(500).json({ message: 'Failed to send email' });
+//   });
+// });
 
-function validateEmail(req, res, next) {
-   if (req.method === 'POST') {
-    req.body('name', 'Name cannot be empty').notEmpty();
-    req.body('email', 'Must be a valid email').isEmail();
-    req.body('message', 'Message cannot be empty').notEmpty();
-  } 
-  const errors = req.validationErrors();
-  if (errors) {
-    return res.status(400).json({
-      message: 'Validation failed',
-      failures: errors
-    });
-  } else {
-    return next();
-  }
-}
+// function validateEmail(req, res, next) {
+//    if (req.method === 'POST') {
+//     req.body('name', 'Name cannot be empty').notEmpty();
+//     req.body('email', 'Must be a valid email').isEmail();
+//     req.body('message', 'Message cannot be empty').notEmpty();
+//   } 
+//   const errors = req.validationErrors();
+//   if (errors) {
+//     return res.status(400).json({
+//       message: 'Validation failed',
+//       failures: errors
+//     });
+//   } else {
+//     return next();
+//   }
+// }
 
-module.exports = router;
+// module.exports = router;
