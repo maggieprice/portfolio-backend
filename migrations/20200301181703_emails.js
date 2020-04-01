6b8e12acbedd66ab612d7a62528a7f039eb3d53e
Justin Price <mr.price@Justins-iMac.local>
2020-03-31 22:38:18 -0400

    commented more out

--- migrations/20200301181703_emails.js
+++ migrations/20200301181703_emails.js
@@ -1,16 +1,16 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('emails', emails => {
-        emails.increments();
+// exports.up = function(knex) {
+//     return knex.schema.createTable('emails', emails => {
+//         emails.increments();
     
-        emails
-          .string('name', 255)
-          .notNullable()
-        emails.string('email').notNullable();
-        emails.string('message').notNullable();
-      });
-};
+//         emails
+//           .string('name', 255)
+//           .notNullable()
+//         emails.string('email').notNullable();
+//         emails.string('message').notNullable();
+//       });
+// };
 
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('emails');
-};
+// exports.down = function(knex) {
+//     return knex.schema.dropTableIfExists('emails');
+// };
